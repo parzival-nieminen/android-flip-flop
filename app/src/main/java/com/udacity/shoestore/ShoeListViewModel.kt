cfad4e78a0ac784a0038c9aa5294d18e9ebc44fe
Parzival <parzival.nieminen@gmail.com>
2021-02-13 17:29:40 +0100

    feat: add basic shoe list item
    * add binding to the shoe item
    * add view binding to the list
    * add viewModel attribute _isListEmpty
    * add cardView to the project
    * add navigation back
    * refactor view, move button's to bottom left corner

--- app/src/main/java/com/udacity/shoestore/ShoeListViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListViewModel.kt
@@ -9,17 +9,24 @@ import timber.log.Timber
 class ShoeListViewModel : ViewModel() {
 
     private val _shoeList = MutableLiveData<MutableList<Shoe>>()
+    private val _isListEmpyt = MutableLiveData<Boolean>()
 
     init {
         Timber.i("init ShoeList ViewModel")
         _shoeList.value = mutableListOf()
+        _isListEmpyt.value = true
+
     }
 
     val shoeList: LiveData<MutableList<Shoe>>
         get() = _shoeList
 
+    val isListEmpty: LiveData<Boolean>
+        get() = _isListEmpyt
+
     fun addShoe(shoe: Shoe) {
         _shoeList.value?.add(shoe)
+        _isListEmpyt.value = false
         Timber.i("new Shoe is added, list size is: " + _shoeList.value?.size)
     }
 }
